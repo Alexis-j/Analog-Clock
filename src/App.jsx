f452962566ea7bd96bfcf51e67383be3bbf8cb47
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-14 12:40:31 +0200

    analog solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,62 @@
-import './index.css'
-
+import React, { useEffect } from 'react';
+import './index.css';
 
 export default function App() {
+  useEffect(() => {
+    const seconds = document.querySelector('.seconds');
+    const minutes = document.querySelector('.minutes');
+    const minute = document.querySelector('.minute');
+    const hour = document.querySelector('.hour');
 
-const seconds = document.querySelector('.seconds');
-const minutes = document.querySelector('.minutes');
-const minute = document.querySelector('.minute');
-const hour = document.querySelector('.hour');
+    // Create spikes
+    for (let s = 0; s < 60; s++) {
+      let mSpikeEl = document.createElement('i');
+      let sSpikeEl = document.createElement('i');
+      mSpikeEl.className = 'spike';
+      sSpikeEl.className = 'spike';
+      mSpikeEl.style = `--rotate:${6 * s}deg`;
+      sSpikeEl.style = `--rotate:${6 * s}deg`;
+      mSpikeEl.setAttribute('data-i', s);
+      sSpikeEl.setAttribute('data-i', s);
 
-// Create spikes
-for(let s = 0; s < 60 ; s++){
-  let mSpikeEl = document.createElement('i');
-  let sSpikeEl = document.createElement('i');
-  mSpikeEl.className = 'spike'
-  sSpikeEl.className = 'spike'
-  mSpikeEl.style = `--rotate:${6 * s}deg`;
-  sSpikeEl.style = `--rotate:${6 * s}deg`;
-  mSpikeEl.setAttribute('data-i', s);
-  sSpikeEl.setAttribute('data-i', s);
-
-  seconds.append(sSpikeEl);
-  minutes.append(mSpikeEl);
-}
-
-function getTime() {
-		let date = new Date(),
-    s  = date.getSeconds() ,
-    m  = date.getMinutes();
-
-  	hour.textContent = date.getHours();
-  	minute.textContent = m;
-
-
-  	minutes.style = `--dRotate:${6 * m}deg`;
-
-    if(s == 0){
-			seconds.classList.add('stop-anim')
-    } else{
-      seconds.classList.remove('stop-anim')
-    }
-    if(m == 0){
-			minutes.classList.add('stop-anim')
-    } else{
-      minutes.classList.remove('stop-anim')
+      seconds.append(sSpikeEl);
+      minutes.append(mSpikeEl);
     }
 
-  		seconds.style = `--dRotate:${6 * s}deg`;
-}
+    function getTime() {
+      let date = new Date(),
+        s = date.getSeconds(),
+        m = date.getMinutes();
 
-setInterval(getTime, 1000);
-getTime();
-  return(
+      hour.textContent = date.getHours();
+      minute.textContent = m;
+
+      minutes.style = `--dRotate:${6 * m}deg`;
+
+      if (s === 0) {
+        seconds.classList.add('stop-anim');
+      } else {
+        seconds.classList.remove('stop-anim');
+      }
+      if (m === 0) {
+        minutes.classList.add('stop-anim');
+      } else {
+        minutes.classList.remove('stop-anim');
+      }
+
+      seconds.style = `--dRotate:${6 * s}deg`;
+    }
+
+    setInterval(getTime, 1000);
+    getTime();
+  }, []);
+
+  return (
     <div className="clock">
       <div className="seconds"></div>
       <div className="minutes"></div>
       <div className="minute">44</div>
       <div className="hour"></div>
     </div>
-
-  )
+  );
 }
